1b990d9d4a0fbc79576407801caf253ed533b7eb
almat101 <sandruozzo@gmail.com>
2025-08-11 18:17:55 +0200

    feat(backend): Add new 'data-processor' FastAPI microservice
    This commit lays the foundation for the new Python microservice responsible for data scraping.
    
    Key changes include:
    - Initial project structure created in the  directory.
    - A  script was added to automate the creation of a Python venv.
    - A basic  file was implemented with a test endpoint to verify the FastAPI server is running correctly.
    - The  file was updated to properly ignore Python-specific artifacts.

---
+++ backend/data-processor/script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Uscita immediata se un comando fallisce
+set -e
+
+echo $PWD
+echo "Creazione dell'ambiente virtuale (.venv)..."
+python3 -m venv .venv
+
+echo "Attivazione dell'ambiente virtuale..."
+source venv/bin/activate
+
+echo "Installazione delle dipendenze..."
+pip install -r requirements.txt
+
+echo "L'ambiente virtuale è stato creato e le dipendenze sono state installate correttamente."
+echo "Per attivarlo, usa: source venv/bin/activate"
