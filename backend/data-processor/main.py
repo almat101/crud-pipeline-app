d28c529e101dd8b65517813ecdfcd8aeb891e0f3
almat101 <sandruozzo@gmail.com>
2025-08-13 16:47:14 +0200

    feat(backend): enhance scraping logic with error handling and cleanup
    - Introduced try-except blocks to handle errors during the scraping process.
    - Added logic to check for missing products and return an appropriate message.
    - Moved browser cleanup logic (teardown(driver)) to the  block to ensure proper resource cleanup in all scenarios.
    - Improved logging for better debugging and monitoring of the scraping process.
    - Ensured that exceptions during WebDriver setup or teardown are logged and re-raised as needed.

--- backend/data-processor/main.py
+++ backend/data-processor/main.py
@@ -3,7 +3,15 @@ from fastapi import FastAPI
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
+SCRAPE_URL= "https://www.subito.it/annunci-italia/vendita/elettronica/?q=thinkpad+t14"
+SELECTOR_PRODUCTS = "div.SmallCard-module_upper-data-group__aRFDu"
+SELECTOR_TITLE = "h2.headline-6.ItemTitle-module_item-title__VuKDo"
+SELECTOR_CITY = "div.PostingTimeAndPlace-module_date-location__1Owcv span"
+SELECTOR_PROVINCE = "div.PostingTimeAndPlace-module_date-location__1Owcv span.caption.small.city"
+SELECTOR_PRICE = "p.index-module_price__N7M2x.x-SmallCard-module_price__yERv7.index-module_small__4SyUf"
 
 app = FastAPI()
 
@@ -20,83 +28,77 @@ def read_root():
 
 @app.get("/scrape")
 def scrape():
-    logger.info("selenium driver start..")
-    # Initialize the Selenium WebDriver and navigate to the target URL
-    driver = setup()
+    try:
+        logger.info("selenium driver start..")
+        # Initialize the Selenium WebDriver and navigate to the target URL
+        driver = setup()
 
-    # Get the title of the current webpage
-    title = driver.title
+        # Get the title of the current webpage
+        title = driver.title
 
-    # Set an implicit wait time of 5 seconds for finding elements
-    driver.implicitly_wait(5)
+        # Set an implicit wait time of 5 seconds for finding elements
+        driver.implicitly_wait(5)
 
-    # find all elements matching the css selector (changed to take the DIV father element)
-    products = driver.find_elements(By.CSS_SELECTOR, "div.SmallCard-module_upper-data-group__aRFDu")
+        # find all elements matching the css selector (changed to take the DIV father element)
+        products = driver.find_elements(By.CSS_SELECTOR, SELECTOR_PRODUCTS)
 
-    # products = driver.find_elements(By.CSS_SELECTOR, "h2.headline-6.ItemTitle-module_item-title__VuKDo")
+        if not products:
+            logger.warning("No products found on the page. Check the CSS selector or page structure.")
+            return {"message": "No products found", "result": []}
 
-    # city = driver.find_elements(By.CSS_SELECTOR, )
-    ##first try returns empty elements
-    # results = [element.text for element in elements]
+        results = []
+        for product in products:
+            try:
+                #Selenium is executing js to extract the title from css query selector using . to connect class name
+                title = driver.execute_script(f"return arguments[0].querySelector('{SELECTOR_TITLE}')?.textContent || '';", product)
+                city = driver.execute_script(f"return arguments[0].querySelector('{SELECTOR_CITY}')?.textContent || '';", product)
+                province = driver.execute_script(f"return arguments[0].querySelector('{SELECTOR_PROVINCE}')?.textContent || '';", product)
+                price = driver.execute_script(f"return arguments[0].querySelector('{SELECTOR_PRICE}')?.textContent || '';", product)
+                # price_html = driver.execute_script("return arguments[0].outerHTML;", product)
+                # logger.info(f"Price HTML: {price_html}")
 
-    ##second try works because is simulating user scroll down on the page like a real user
-    # results = []
-    # for element in elements:
-    #     # Scroll to the element
-    #     ActionChains(driver).move_to_element(element).perform()
-    #     # Extract the text
-    #     if element.text.strip():  # Ensure the text is not empty
-    #         results.append(element.text)
+                product_data = {
+                    "title": title.strip(),
+                    "city": city.strip(),
+                    "province": province.strip(),
+                    "price": price.strip(),
+                }
+                results.append(product_data)
 
-    ##third try wotks but is using javascript to extract elements ( faster DOM manipulation, doesn't mimic real user)
-    results = []
-    for product in products:
-        #Selenium is executing js to extract the title from css query selector using . to connect class name
-        title = driver.execute_script("return arguments[0].querySelector('h2.headline-6.ItemTitle-module_item-title__VuKDo')?.textContent || '';", product)
-        city = driver.execute_script("return arguments[0].querySelector('div.PostingTimeAndPlace-module_date-location__1Owcv span')?.textContent || '';", product)
-        province = driver.execute_script("return arguments[0].querySelector('div.PostingTimeAndPlace-module_date-location__1Owcv span.caption.small.city')?.textContent || '';", product)
-        price = driver.execute_script("return arguments[0].querySelector('p.index-module_price__N7M2x.x-SmallCard-module_price__yERv7 index-module_small__4SyUf')?.textContent || '';", product)
-        # price_html = driver.execute_script("return arguments[0].outerHTML;", product)
-        # logger.info(f"Price HTML: {price_html}")
+            except Exception as e:
+                # Log the error for this product and continue with the next one
+                logger.error(f"Error scraping product: {e}")
+                continue
 
-        product_data = {
-            "title": title.strip(),
-            "city": city.strip(),
-            "province": province.strip(),
-            "price": price.strip(),
-        }
-        
-        results.append(product_data)
+        # Close the browser and clean up resources moved to finally block 
+        #teardown(driver)
 
-    # text_box = driver.find_element(by=By.ID, value="my-text-id")
-    # Locate the submit button using a CSS selector
-    # submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button")
-
-    # # Enter the text "Selenium" into the text input field
-    # text_box.send_keys("Selenium")
-    # # Click the submit button to submit the form
-    # submit_button.click()
+        # Log a message indicating the scraping process was completed successfully
+        logger.info(f"Scraping completed succesfully: {results}")
+        # Return a JSON response with a success message and the retrieved text
+        return {"message": "Scrape success", "result": results}
     
-    # # Locate the element with the ID "message" and retrieve its text content
-    # message = driver.find_element(by=By.ID, value="message")
-    # text = message.text  # Extract the text from the located element
-
-    # Close the browser and clean up resources
-    teardown(driver)
-
-    # Log a message indicating the scraping process was completed successfully
-    logger.info(f"Scraping completed succesfully: {results}")
-    # Return a JSON response with a success message and the retrieved text
-    return {"message": "Scrape success", "result": results}
-
+    except Exception as e:
+        logger.error(f"Error during scrape: {e}")
+        return {"message": "scrape failed", "error" : str(e)}
+    finally:
+        if(driver):
+            teardown(driver)
 
 def setup():
-    # Initialize the Selenium WebDriver (Chrome) and navigate to the target URL
-    driver = webdriver.Chrome()
-    driver.get("https://www.subito.it/annunci-italia/vendita/elettronica/?q=thinkpad+t14")
-    return driver  # Return the WebDriver instance
+    try:
+        # Initialize the Selenium WebDriver (Chrome) and navigate to the target URL
+        driver = webdriver.Chrome()
+        driver.get(SCRAPE_URL)
+        return driver  # Return the WebDriver instance
+    except Exception as e:
+        logger.error(f"Error on setup webdriver: {e}")
+        raise # Re-raises the same exception
 
 
 def teardown(driver):
-    # Quit the WebDriver and close the browser
-    driver.quit()
+    try:
+        # Quit the WebDriver and close the browser
+        driver.quit()
+    except Exception as e:
+        logger.error(f"Error during WebDriver teardown: {e}")
